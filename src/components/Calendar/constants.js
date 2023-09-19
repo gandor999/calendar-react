a964a796f21f0a66fcb23d6772005c83ca2423c5
Geodor Ruales <gandor999@gmail.com>
2023-09-19 23:03:03 +0800

    refactor: put constant vars into separate file

---
+++ src/components/Calendar/constants.js
@@ -0,0 +1,18 @@
+const nameOfMonths = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+]
+
+const daysOfWeeks = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+
+export { nameOfMonths, daysOfWeeks }
